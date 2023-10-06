8cf10f357184c6a930a96468e3f0385861b8b342
Lucas Ferruccio <lucaspaiva.ufrpe@gmail.com>
2023-10-06 12:29:14 -0300

    Testando a criação dos arquivos Conta e Programa .java

---
+++ Conta.java
@@ -0,0 +1,29 @@
+package testando;
+
+public class Conta {
+	
+	private String numero;
+	private double saldo;
+	
+	public Conta(String numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	public void creditar(double valor) {
+		saldo = saldo + valor;
+	}
+	
+	public void debitar(double valor) {
+		saldo = saldo - valor;
+	}
+	
+	public void transferir(Conta conta1, Conta conta2, double valor) {
+		conta1.saldo = conta1.saldo - valor;
+		conta2.saldo = conta2.saldo + valor;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+}
---
+++ Programa.java
@@ -0,0 +1,26 @@
+package testando;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Conta conta1 = new Conta("1234", 20.0);
+		Conta conta2 = new Conta("1235", 30.0);
+		
+		// Creditando
+		System.out.println("Creditar:");
+		conta1.creditar(10);
+		System.out.println(conta1.getSaldo());
+		
+		// Debitando
+		System.out.println("Debitar:");
+		conta2.debitar(10);
+		System.out.println(conta2.getSaldo());
+		
+		// Transferindo
+		System.out.println("Transferir:");
+		conta1.transferir(conta1, conta2, 10);
+		System.out.println(conta1.getSaldo());
+		System.out.println(conta2.getSaldo());
+		
+	}
+}
